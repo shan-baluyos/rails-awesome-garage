0a6afc8cf58ad56a0e790bffe21b9991114849d7
Shan Baluyos <shan.baluyos21@gmail.com>
2024-10-03 11:28:43 +0100

    Step 4 validations done

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
   belongs_to :owner
+  has_many :reviews
+  has_many :favourites
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :year, presence: true, numericality: { only_integer: true }
+  validates :fuel, presence: true
 end
